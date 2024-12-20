41b24320fdb1331c30599f4aadc16c86cff9f8f0
muwomotapiwa <muwomotapiwa@gmail.com>
2024-12-20 20:30:46 +0200

    changes

--- js/styles.js
+++ js/styles.js
@@ -1,20 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Set the logo image
+    // Set the logo image dynamically
     const logo = document.getElementById('logo');
     logo.src = 'assets/images/MoCrest-logo.jpeg';
 
-    // Set background images for sections dynamically
-    const sections = [
-        { id: 'about', background: 'assets/images/Index-background.jpeg' },
-        { id: 'subsidiaries', background: 'assets/images/Index-background.jpeg' },
-        { id: 'corporate-responsibility', background: 'assets/images/Index-background.jpeg' },
-        { id: 'contact-us', background: 'assets/images/Index-background.jpeg' },
-    ];
-
-    sections.forEach(section => {
-        const sectionElement = document.getElementById(section.id);
-        if (sectionElement) {
-            sectionElement.style.backgroundImage = `url('${section.background}')`;
-        }
-    });
+    // No need to set background images dynamically, as it's handled by CSS
 });
